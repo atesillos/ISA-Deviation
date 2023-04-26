49cf9293bce1840d0878a1352ed6591e23fc40fd
Angel Tesillos <atesillos7@gmail.com>
2023-04-26 22:22:25 +0000

    main.java methods created

--- src/main.java
+++ src/main.java
@@ -1,5 +1,31 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Code works!");
+        System.out.println("Hello, world!");
+        int height = 26000;
+        double actualTemp = -30;
+        double stdTemp = calcST(height);
+        double isaT = calcISATemp(actualTemp, stdTemp);
+        System.out.println(
+            "Alititude: " + height + 
+            "\nActual Temp: " + actualTemp + 
+            "\nStandard Temp: " + stdTemp + 
+            "\nISA Temp: " + isaT);
+    } // main method
+
+    /*
+    * ISA temp at a certain altitude.
+    * Height is to be measured in feet.
+    * @param height 
+    */
+    static double calcST(int alt) {
+        return (15 - ((alt/1000) * 1.98));
     }
-}
+
+    /*
+    * ISA temperature deviation
+    */
+    static double calcISATemp (double AT, double ST) {
+        return (ST - AT);
+    }
+    
+} // main class
